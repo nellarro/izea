9f6c07f505953b3b49309a36e79e199be3675fff
Jenell Pizarro <nellarro@gmail.com>
2017-11-10 13:29:26 -0500

    donezo

--- app/components/diary-comp.js
+++ app/components/diary-comp.js
@@ -1,10 +1,36 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    text: '',
     keyphrase: '',
+    encoded: '',
     actions: {
-        handleChange(keyphrase) {
-            window.console.log("keyphrase: " + keyphrase)
+        handleSubmit(text, keyphrase, event) {
+            event.preventDefault();
+            this.set('encoded', this.vigenere(text, keyphrase))
         }
-    } 
+    },
+    letterToKeyCode (letter) {
+        return letter.charCodeAt(0) - 65;
+    },
+    
+    // the actual cipher
+    vigenere (text, keyword, decode) {
+        let i = 0,
+            b;
+        keyword = keyword.toUpperCase().replace(/[^A-Z]/g, "");
+        return text
+            .toUpperCase()
+            .replace(/[^A-Z]/g, "")
+            .replace(/[A-Z]/g, letter => {
+                b = keyword[i++ % keyword.length];
+                return String.fromCharCode(
+                    (this.letterToKeyCode(letter) +
+                        (decode ? 26 - this.letterToKeyCode(b) : this.letterToKeyCode(b))) %
+                        26 +
+                        65
+                )
+            })
+    }
+     
 });
